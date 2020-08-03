5b2be32fbc16f333663f0736228dac342a7f06d7
dataabyss <dataabyss@users.noreply.github.com>
2020-08-02 19:50:17 -0700

    ring_buffer and names code

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,39 @@
-class RingBuffer:
-    def __init__(self, capacity):
-        pass
+# class RingBuffer:
+#     def __init__(self, capacity):
+#         pass
 
-    def append(self, item):
-        pass
+#     def append(self, item):
+#         pass
+
+#     def get(self):
+#         pass
+
+class RingBuffer:
+    """ class that implements a not-yet-full buffer """
+    def __init__(self,capacity): # size max > capacity
+        self.capacity = capacity
+        self.data = []
+
+    # class __Full(RingBuffer):
+    #     """ class that implements a full buffer """
+    #     def append(self, x):
+    #         """ Append an element overwriting the oldest one. """
+    #         self.data[self.cur] = x
+    #         self.cur = (self.cur+1) % self.max
+    #     def get(self):
+    #         """ return list of elements in correct order """
+    #         return self.data[self.cur:]+self.data[:self.cur]
+
+    def append(self,item): # x > item
+        """append an element at the end of the buffer"""
+        self.data.append(item)
+        # if len(self.data) == self.capacity:
+        #     self.cur = 0
+        #     self.data[self.cur] = item #
+        #     self.cur = (self.cur+1) % self.capacity #            
+            # Permanently change self's class from non-full to full
+            # self.__class__ = self.__Full
 
     def get(self):
-        pass
+        """ Return a list of elements from the oldest to the newest. """
+        return self.data
