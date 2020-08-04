46fffd7fd1ac5b2f3e8f46ec4ca2d86e99fba1dd
dataabyss <dataabyss@users.noreply.github.com>
2020-08-04 16:16:00 -0700

    part2

--- names/names.py
+++ names/names.py
@@ -18,19 +18,49 @@ duplicates = []  # Return the list of duplicates in this data structure
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
+# def repeats(x, y):
+#     for name1, name2 in x, y:
+#         if name1 != name2:
+#             return
+#         else:
+#             duplicates.append(x)
+#             repeats(x,y)
+
+# def repeats(x, y):
+#     for x in x, y: # if
+#         if x != y:
+#             return
+#         else:
+#             duplicates.append(x)
+#             repeats(x,y)
+
+# def repeats(x, y):
+#     for item1 in x:
+#         for item2 in y:
+#             if item1 == item2:
+#                 duplicates.append(item1)
+#             else:
+#                 repeats(x, y)
+
+# def repeats(a, b):
+#     a_set = set(names_1)
+#     b_set = set(names_2)
+
+#     if (a_set & b_set):
+#         duplicates.append(a_set)
+#     else:
+#         repeats(a,b)
+
 def repeats(x, y):
-    for name1, name2 in x, y:
-        if name1 != name2:
-            return
-        else:
-            duplicates.append(x)
-            repeats(x,y)
+    dup_values = [duplicates.append(elem) for elem in x if elem in y]
+    return dup_values
 
 repeats(names_1, names_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print (f"\n{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds\n")
+print(f'The runtime for the starter code was O(n)\n')
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
